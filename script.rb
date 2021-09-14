98fe037d8cad54a50d9e9a5fbf3740555c715d0f
Koki Miyazaki <miyazaki@shuttlerock.co.jp>
2021-09-14 13:37:56 +0900

    Skip aproving if the user already approved the PR

--- script.rb
+++ script.rb
@@ -8,8 +8,8 @@ REPO_NAME = ENV['REPO_NAME']
 
 pr_numbers = [7603, 7605, 7611]
 
-# @param pr_numbers [Array] the PR numbers to comment
-# @param comment [String] the comment message
+# @param pr_numbers [Array] The PR numbers to comment
+# @param comment [String] The comment message
 def comment_prs(pr_numbers, comment)
   pr_numbers.each do |num|
     comment_url = 'https://api.github.com/repos/' + REPO_NAME + '/issues/' + num.to_s + '/comments'
@@ -18,11 +18,24 @@ def comment_prs(pr_numbers, comment)
   end
 end
 
-# @param pr_numbers [Array] the PR numbers to approve
+# @param pr_numbers [Array] The PR numbers to approve
 def approve_prs(pr_numbers)
   pr_numbers.each do |num|
+    next if latest_review(num)&.state == 'APPROVED'
+
     review_url = 'https://api.github.com/repos/' + REPO_NAME + '/pulls/' + num.to_s + '/reviews'
 
     res = @client.post(review_url, event: 'APPROVE')
   end
+end
+
+# @return [nil|Hash] The latest review object if exists, nil otherwise.
+# @param [String] The pr number to get review of.
+def latest_review(pr_num)
+  review_url = 'https://api.github.com/repos/' + REPO_NAME + '/pulls/' + pr_num.to_s + '/reviews'
+
+  res = @client.get(review_url)
+  res.filter do |review|
+    review.user.login == @client.user.login
+  end.last
 end
