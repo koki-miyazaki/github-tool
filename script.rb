697b5eecd55e383b7a96b00e2606a7409d849111
miyazaki koki <miyazakikoki@sr-mk.local>
2021-11-08 17:40:59 +0900

    Skip approving when the PR is not open

--- script.rb
+++ script.rb
@@ -6,36 +6,50 @@ require 'octokit'
 
 REPO_NAME = ENV['REPO_NAME']
 
-pr_numbers = [7603, 7605, 7611]
-
 # @param pr_numbers [Array] The PR numbers to comment
 # @param comment [String] The comment message
-def comment_prs(pr_numbers, comment)
+def comment_prs(pr_numbers = [], comment)
   pr_numbers.each do |num|
-    comment_url = 'https://api.github.com/repos/' + REPO_NAME + '/issues/' + num.to_s + '/comments'
-
-    res = @client.post(comment_url, body: comment)
+    res = @client.post(comment_url(num), body: comment)
   end
 end
 
+# Public: Approve open PRs
+#
 # @param pr_numbers [Array] The PR numbers to approve
-def approve_prs(pr_numbers)
+def approve_prs(pr_numbers = [])
   pr_numbers.each do |num|
+    next if pr_status(num) != 'open'
     next if latest_review(num)&.state == 'APPROVED'
 
-    review_url = 'https://api.github.com/repos/' + REPO_NAME + '/pulls/' + num.to_s + '/reviews'
-
-    res = @client.post(review_url, event: 'APPROVE')
+    res = @client.post(reviews_url(num), event: 'APPROVE')
   end
 end
 
 # @return [nil|Hash] The latest review object if exists, nil otherwise.
 # @param [String] The pr number to get review of.
 def latest_review(pr_num)
-  review_url = 'https://api.github.com/repos/' + REPO_NAME + '/pulls/' + pr_num.to_s + '/reviews'
-
-  res = @client.get(review_url)
+  res = @client.get(reviews_url(pr_num))
   res.filter do |review|
     review.user.login == @client.user.login
   end.last
+end
+
+def pr_status(pr_num)
+  @client.get(pr_url(pr_num))[:state]
+end
+
+# e.g. https://api.github.com/repos/some/repo/pulls/123/reviews
+def reviews_url(pr_num)
+  pr_url(pr_num) + '/reviews'
+end
+
+# e.g. https://api.github.com/repos/some/repo/pulls/123
+def pr_url(pr_num)
+  'https://api.github.com/repos/' + REPO_NAME + '/pulls/' + pr_num.to_s
+end
+
+# e.g. https://api.github.com/repos/some/repo/issues/123/comments
+def comments_url(pr_num)
+  'https://api.github.com/repos/' + REPO_NAME + '/issues/' + num.to_s + '/comments'
 end
